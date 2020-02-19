5c6c3932d3d584b178eb073270eb985286a32f19
Igor Chechura <igorchechura@gmail.com>
2020-02-19 15:08:56 +0300

    Routers

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
 	return (
-		<div className={classes.content}>
+		<div>
 			<div>
 				<img src="https://images.unsplash.com/photo-1500622944204-b135684e99fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"/>
 			</div>
