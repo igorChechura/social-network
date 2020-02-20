6b10ead1c322ebc2547b56d7be232904d25c24b7
Igor Chechura <igorchechura@gmail.com>
2020-02-20 17:35:59 +0300

    UI & BLL

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,15 +4,15 @@ import Post from './Post/Post';
 
 const MyPosts = (props) => {
 
-	let posts = [
+	/*let posts = [
 		{id: 1, message: 'Hi, how are you?', likesCount: 25},
 		{id: 2, message: 'It\'s my first post', likesCount: 15},
 		{id: 2, message: 'It\'s my first post', likesCount: 15},
 		{id: 2, message: 'It\'s my first post', likesCount: 15},
 		{id: 2, message: 'It\'s my first post', likesCount: 15}
-	];
+	];*/
 
-	let postsElements = posts.map( p => <Post message={p.message} likesCount={p.likesCount} /> );
+	let postsElements = props.posts.map( p => <Post message={p.message} likesCount={p.likesCount} /> );
 
 	return (
 		<div className={classes.postsBlock}>
