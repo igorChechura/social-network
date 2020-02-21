eb4e7332f617c0beeac86ae725393ed2883cbfbc
Igor Chechura <igorchechura@gmail.com>
2020-02-21 13:32:32 +0300

    Refact, state

---
+++ src/redux/state.js
@@ -0,0 +1,29 @@
+let state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: 'Hi, how are you?', likesCount: 25},
+            {id: 2, message: 'It\'s my first post', likesCount: 15},
+            {id: 2, message: 'It\'s my first post', likesCount: 15},
+            {id: 2, message: 'It\'s my first post', likesCount: 15},
+        ]
+    },
+    dialogsPage: {
+        dialogs: [
+            {id: 1, name: 'Dimych'},
+            {id: 2, name: 'Andrey'},
+            {id: 3, name: 'Sveta'},
+            {id: 4, name: 'Sasha'},
+            {id: 5, name: 'Viktor'},
+            {id: 6, name: 'Valera'}
+        ],
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'How are you'},
+            {id: 3, message: 'Yo!'},
+            {id: 4, message: 'Yo!'},
+            {id: 5, message: 'Yo!'}
+        ]
+    }
+};
+
+export default state;
