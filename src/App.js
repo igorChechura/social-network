b0a794404bb09d27ada579742746041aaf58a24a
Igor Chechura <igorchechura@gmail.com>
2020-02-24 12:50:17 +0300

    Ref

--- src/App.js
+++ src/App.js
@@ -11,20 +11,20 @@ import {BrowserRouter, Route} from "react-router-dom";
 
 const App = (props) => {
 	return (
-			<BrowserRouter>
-				<div className='app-wrapper'>
-					<Header />
-					<Navbar />
-					<div className='app-wrapper-content'>
-						<Route path='/profile' render={ () => <Profile state={props.state.profilePage} /> } />
-						<Route path='/dialogs' render={ () => <Dialogs state={props.state.dialogsPage} /> } />
-						<Route path='/news' render={ () => <News /> } />
-						<Route path='/music' render={ () => <Music /> } />
-						<Route path='/settings' render={ () => <Settings /> } />
-					</div>
-				</div>
-			</BrowserRouter>
-		);
-	};
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header />
+                <Navbar />
+                <div className='app-wrapper-content'>
+                    <Route path='/profile' render={ () => <Profile state={props.state.profilePage} /> } />
+                    <Route path='/dialogs' render={ () => <Dialogs state={props.state.dialogsPage} /> } />
+                    <Route path='/news' render={ () => <News /> } />
+                    <Route path='/music' render={ () => <Music /> } />
+                    <Route path='/settings' render={ () => <Settings /> } />
+                </div>
+            </div>
+        </BrowserRouter>
+    );
+};
 
 export default App;
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -8,6 +8,13 @@ const Dialogs = (props) => {
     let dialogsElements = props.state.dialogs.map( d => <DialogItem name={d.name} id={d.id} /> );
     let messagesElements = props.state.messages.map( m => <Message message={m.message} /> );
 
+    let newMessageElement = React.createRef();
+
+    let addMessage = () => {
+        let text = newMessageElement.current.value;
+        alert(text);
+    };
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
@@ -15,6 +22,8 @@ const Dialogs = (props) => {
             </div>
             <div className={classes.messages}>
                 { messagesElements }
+                <textarea ref={newMessageElement}></textarea>
+                <button onClick={addMessage}>Add message</button>
             </div>
         </div>
     );
