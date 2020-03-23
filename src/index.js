fe328d2e88832604a94078d71d27e009c590fbc3
Igor Chechura <igorchechura@gmail.com>
2020-03-23 17:33:28 +0300

    StoreContext

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import store from "./redux/redux-store";
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from "./StoreContext";
 
 
 let rerenderEntireTree = (state) => {
     debugger;
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} dispatch={store.dispatch.bind(store)} store={store} />
+            <Provider store={store}>
+                <App />
+            </Provider>
         </BrowserRouter>, document.getElementById('root'));
 };
 
